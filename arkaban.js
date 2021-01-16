417dbebf02bcb0ff047d44bfc1ab1c26028bf913
Arkajit  Bandyopadhyay <46055684+ArkajitBanerjee@users.noreply.github.com>
2021-01-16 15:25:13 +0530

    Java script demo
    these are   samples code of  java  scripts

---
+++ arkaban.js
@@ -0,0 +1,24 @@
+var  a=4;
+switch(a)
+{
+    case 1 :
+        console.log("The value of a is 1");
+    break;
+    case 2: 
+    console.log("The value of a is 2");
+    break;
+    case 3:
+        console.log("The value of a is 3");
+    break;
+    case 4:
+        console.log("The value of a is 4");
+    break;
+    case 5:
+        console.log("The value of a is 5");
+    break;
+    default : 
+    console.log("The value of a is not found");
+    break;
+
+}
+
